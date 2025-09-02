9f6c55fe42f7a51db5f226cb0d3d2d6fc498791f
Valeri <35935215+Valerian1964@users.noreply.github.com>
2025-09-02 18:44:36 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/walletbridge/main.go
@@ -0,0 +1,23 @@
+// cmd/walletbridge/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "walletbridge/internal/walletbridge"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := walletbridge.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
